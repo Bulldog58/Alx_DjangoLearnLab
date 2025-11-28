a4d486d00be15d306fc98dda42c4ff3792a65859
Bulldog58 <felloh99@gmail.com>
2025-11-28 15:21:57 +0300

    third commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register 
-from django.contrib.auth import views as auth_views # Import Django's built-in auth views
+from .views import list_books, LibraryDetailView, register # <-- Make sure 'register' is imported
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
     # Application Views
@@ -9,16 +9,12 @@ urlpatterns = [
     
     # --- Authentication Views ---
     
-    # Registration View (Custom FBV)
-    # This correctly links to the 'register' function in .views
+    # Registration View (Fix: links to views.register)
     path('register/', register, name='register'),
     
     # Login View (Built-in CBV)
-    # Uses the LoginView class and points it to your login.html template
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     
     # Logout View (Built-in CBV)
-    # Uses the LogoutView class and points it to your logout.html template
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'), 
-    # NOTE: You can also use next_page='login' instead of template_name if you prefer
 ]
