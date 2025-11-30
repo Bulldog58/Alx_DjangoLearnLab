8adb2051a86628acc280aa49497ec57c01c2ed6e
Bulldog58 <felloh99@gmail.com>
2025-11-30 14:18:21 +0300

    initial commit

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,19 +1,24 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# --- Customize the Admin Interface ---
-class BookAdmin(admin.ModelAdmin):
-    # 1. Customize the List View Display: fields shown on the main table
-    list_display = ('title', 'author', 'publication_year')
+class CustomUserAdmin(UserAdmin):
+    """Admin panel configuration for CustomUser."""
+    model = CustomUser
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal Info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo', 'is_staff', 'is_active'),
+        }),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('email',)
 
-    # 2. Configure List Filters: sidebar filters for quick sorting
-    list_filter = ('author', 'publication_year')
-
-    # 3. Configure Search Capabilities: fields searchable via the admin search box
-    search_fields = ('title', 'author', 'publication_year')
-
-    # Optional: Fields to make clickable links to the edit page
-    list_display_links = ('title',)
-
-# Register the Book Model with the Django Admin
-admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
